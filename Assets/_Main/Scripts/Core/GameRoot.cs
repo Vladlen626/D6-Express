476a9923df89e68195eca050571ebea81be13d29
LeN <vladlenfrog@gmail.com>
2025-12-28 00:43:04 +0300

    Dice spawn

--- Assets/_Main/Scripts/Core/GameRoot.cs
+++ Assets/_Main/Scripts/Core/GameRoot.cs
@@ -1,5 +1,11 @@
+using System.Collections.Generic;
+using _Main.Scripts.Dice;
 using Cysharp.Threading.Tasks;
 using PlatformCore.Core;
+using PlatformCore.Services;
+using PlatformCore.Services.Factory;
+using PlatformCore.Services.Factory.PlatformCore.Services.Factory;
+using UnityEngine;
 
 namespace _Main.Scripts.Core
 {
@@ -7,12 +13,59 @@ namespace _Main.Scripts.Core
 	{
 		protected override void RegisterServices(PersistentSceneContext persistentSceneContext)
 		{
-			throw new System.NotImplementedException();
+			Debug.Log("[GameRoot] Register services...");
+
+			var logger = new LoggerService();
+			var resourceService = new ResourceService(logger);
+			var objectFactory = new ObjectFactory(resourceService, logger);
+			var sceneService = new SceneService(logger);
+
+			_serviceLocator.Register<ILoggerService, LoggerService>(logger);
+			_serviceLocator.Register<IResourceService, ResourceService>(resourceService);
+			_serviceLocator.Register<IObjectFactory, ObjectFactory>(objectFactory);
+			_serviceLocator.Register<ISceneService, SceneService>(sceneService);
+
+			Debug.Log("[GameRoot] Services registered!");
 		}
 
-		protected override UniTask LaunchGameAsync(PersistentSceneContext persistentSceneContext)
+		protected override async UniTask LaunchGameAsync(PersistentSceneContext persistentSceneContext)
 		{
-			throw new System.NotImplementedException();
+			var factory = _serviceLocator.Get<IObjectFactory>();
+			var logger = _serviceLocator.Get<ILoggerService>();
+			var sceneService = _serviceLocator.Get<ISceneService>();
+			
+			var activeSceneName = sceneService.GetActiveSceneName();
+			await UniTask.WaitUntil(() => sceneService.IsSceneLoaded(activeSceneName));
+
+			if (!sceneService.TryGetSceneContext("PersistentScene", out var context))
+			{
+				logger.LogError($"[GameRoot] SceneContext not found in scene '{activeSceneName}'!");
+				return;
+			}
+
+			var sceneContext = context as SceneContext;
+			if (sceneContext == null)
+			{
+				logger.LogError($"[GameRoot] SceneContext mistype'{activeSceneName}'!]");
+			}
+
+			var diceViews =
+				await DiceFactory.SpawnDiceArrayAsync(factory, sceneContext.DicePositionsHandler.DicePositions);
+			
+			var diceControllers = new List<DiceController>();
+
+			foreach (var diceView in diceViews)
+			{
+				var model = new DiceModel(new LoadedDiceProfileConfig());
+				var controller = new DiceController(model, diceView);
+				diceControllers.Add(controller);
+			}
+
+			foreach (var diceController in diceControllers)
+			{
+				await _lifecycle.RegisterAsync(diceController);
+			}
+
 		}
 	}
 }
---
+++ Assets/_Main/Scripts/Dice/DiceController.cs
@@ -0,0 +1,85 @@
+﻿using PlatformCore.Core;
+using PlatformCore.Infrastructure.Lifecycle;
+using UnityEngine;
+
+namespace _Main.Scripts.Dice
+{
+	public class DiceController : IBaseController, IActivatable, IDeactivatable
+	{
+		private readonly DiceModel diceModel;
+		private readonly DiceView diceView;
+
+		public DiceController(DiceModel inDiceModel, DiceView inDiceView)
+		{
+			diceModel = inDiceModel;
+			diceView = inDiceView;
+		}
+		public void Activate()
+		{
+			Roll();
+			diceView.OnDiceClicked += HandleDiceClicked;
+		}
+
+		public void Deactivate()
+		{
+			if (diceView != null)
+			{
+				diceView.OnDiceClicked -= HandleDiceClicked;
+			}
+		}
+
+		public void Roll()
+		{
+			float[] weights = diceModel.Profile.Weights;
+			int value = GetWeightedRandomValue(weights);
+
+			diceModel.SetValue(value);
+			diceView.SetSideMesh(value);
+		}
+
+		private void HandleDiceClicked()
+		{
+			if (diceModel.IsSaved) return;
+
+			diceModel.SetChosen(!diceModel.IsChosen);
+			diceView.UpdateChosenVisual(diceModel.IsChosen);
+		}
+
+		public void Save()
+		{
+			diceModel.SetSaved(true);
+			diceModel.SetChosen(false);
+			diceView.UpdateChosenVisual(false);
+		}
+
+		public void Reset()
+		{
+			diceModel.Reset();
+			diceView.SetSideMesh(0);
+			diceView.UpdateChosenVisual(false);
+		}
+
+		private int GetWeightedRandomValue(float[] weights)
+		{
+			float totalWeight = 0f;
+			foreach (float weight in weights)
+			{
+				totalWeight += weight;
+			}
+
+			float randomValue = Random.Range(0f, totalWeight);
+			float cumulativeWeight = 0f;
+
+			for (int i = 0; i < weights.Length; i++)
+			{
+				cumulativeWeight += weights[i];
+				if (randomValue <= cumulativeWeight)
+				{
+					return i + 1;
+				}
+			}
+
+			return 1;
+		}
+	}
+}
