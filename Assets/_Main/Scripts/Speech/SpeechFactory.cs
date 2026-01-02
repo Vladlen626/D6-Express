0fc81ed0bfcb5dac855bb6ce5e87e5a85cf3600a
basarab <joker-power@yandex.ru>
2026-01-02 22:25:48 +0300

    funny texts

--- Assets/_Main/Scripts/Speech/SpeechFactory.cs
+++ Assets/_Main/Scripts/Speech/SpeechFactory.cs
@@ -26,11 +26,20 @@ public static class SpeechFactory
 		var speechNodeComradeText = new SpeechNodeShowTextLine("Я вас категорически приветствую!").Init(speechPassengerGeneric);
 		speechPassengerComrade.SetRootNode(speechNodeComradeText);
 
+		var speechPassengerAnecdote = new Speech(3);
+		var speechNodeAnecdoteText1 = new SpeechNodeShowTextLine("Заходит пациент к доктору и говорит").Init(speechPassengerAnecdote);
+		var speechNodeAnecdoteText2 = new SpeechNodeShowTextLine("Доктор, у меня член чешется").After(speechNodeAnecdoteText1).Init(speechPassengerAnecdote);
+		var speechNodeAnecdoteText3 = new SpeechNodeShowTextLine("Доктор отвечает \"Мой чаще\"").After(speechNodeAnecdoteText2).Init(speechPassengerAnecdote);
+		var speechNodeAnecdoteText4 = new SpeechNodeShowTextLine("А пациент такой \"Нет, мой\"").After(speechNodeAnecdoteText3).Init(speechPassengerAnecdote);
+		var speechNodeAnecdoteText5 = new SpeechNodeShowTextLine("Ха-ха-ха-ха!").After(speechNodeAnecdoteText4).Init(speechPassengerAnecdote);
+		speechPassengerAnecdote.SetRootNode(speechNodeAnecdoteText1);
+
 		var speeches = new Speech[]
 		{
 			speechBuyTicket,
 			speechPassengerGeneric,
-			speechPassengerComrade
+			speechPassengerComrade,
+			speechPassengerAnecdote
 		};
 
 		var speechModel = new SpeechModel(speeches);
