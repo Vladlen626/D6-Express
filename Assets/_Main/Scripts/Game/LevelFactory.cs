7b3cfcdaa11709e4d7d5bc8adb5df40def0a2a7d
LeN <vladlenfrog@gmail.com>
2026-01-03 23:47:45 +0300

    Fix tabs

--- Assets/_Main/Scripts/Game/LevelFactory.cs
+++ Assets/_Main/Scripts/Game/LevelFactory.cs
@@ -5,30 +5,32 @@ using PlatformCore.Services.UI;
 
 public static class LevelFactory
 {
-    public static LevelModel CreateLevelModel(InventoryModel inventoryModel)
-    {
-        // TODO: в настройки
-        var ticksPerDay = 3;
-        int days = 3;
-        int cashGoal = 500;
+	public static LevelModel CreateLevelModel(InventoryModel inventoryModel)
+	{
+		// TODO: в настройки
+		var ticksPerDay = 3;
+		int days = 3;
+		int cashGoal = 500;
 
-        var levelModel = new LevelModel(ticksPerDay, days, cashGoal, inventoryModel);
-        return levelModel;
-    }
+		var levelModel = new LevelModel(ticksPerDay, days, cashGoal, inventoryModel);
+		return levelModel;
+	}
 
-    public static IEnumerable<IBaseController> GetBaseControllers(SceneContext sceneContext, IUIService uiService,
-        LevelModel levelModel, PlayerModel playerModel, DiceGameModel diceGameModel, PlayerView playerView)
-    {
-        yield return new LevelViewController(uiService, levelModel, sceneContext.Sun, sceneContext.TrainBlock, sceneContext.StationBlock, playerView, sceneContext.PlayerTrainSpawnPosition, sceneContext.PlayerStationSpawnPosition);
-        yield return new LevelController(levelModel, playerModel, diceGameModel);
-    }
+	public static IEnumerable<IBaseController> GetBaseControllers(SceneContext sceneContext, IUIService uiService,
+		LevelModel levelModel, PlayerModel playerModel, DiceGameModel diceGameModel, PlayerView playerView)
+	{
+		yield return new LevelViewController(uiService, levelModel, sceneContext.Sun, sceneContext.TrainBlock,
+			sceneContext.StationBlock, playerView, sceneContext.PlayerTrainSpawnPosition,
+			sceneContext.PlayerStationSpawnPosition);
+		yield return new LevelController(levelModel, playerModel, diceGameModel);
+	}
 
-    public static IEnumerable<IBaseController> GetSleepControllers(LevelModel levelModel, PlayerView playerView)
-    {
-        var sleepView = playerView.GetComponent<SleepView>();
-        var interactor = playerView.GetComponent<Interactor>();
+	public static IEnumerable<IBaseController> GetSleepControllers(LevelModel levelModel, PlayerView playerView)
+	{
+		var sleepView = playerView.GetComponent<SleepView>();
+		var interactor = playerView.GetComponent<Interactor>();
 
-        var sleepController = new SleepController(levelModel, sleepView, interactor);
-        yield return sleepController;
-    }
+		var sleepController = new SleepController(levelModel, sleepView, interactor);
+		yield return sleepController;
+	}
 }
