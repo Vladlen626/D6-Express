cd13ba37aaa086066113f858d9431fea13eb8348
basarab <joker-power@yandex.ru>
2026-01-10 11:32:18 +0300

    Sitting anim support removed (temp)

--- Assets/_Main/Scripts/Player/CharacterStateHandlerSitting.cs
+++ Assets/_Main/Scripts/Player/CharacterStateHandlerSitting.cs
@@ -5,18 +5,4 @@ using UnityEngine;
 public class CharacterStateHandlerSitting : CharacterStateHandler
 {
 	public override CharacterState State => CharacterState.SITTING;
-
-	protected override void EnterInternal()
-	{
-		// base.EnterInternal();
-
-		CharacterView.GetComponent<Animator>().SetInteger("State", (int)CharacterState.SITTING);
-	}
-
-	protected override void ExitInternal()
-	{
-		CharacterView.GetComponent<Animator>().SetInteger("State", (int)CharacterState.DEFAULT);
-
-		// base.ExitInternal();
-	}
 }
